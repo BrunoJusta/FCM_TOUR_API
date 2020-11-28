738370985ce326124153f7c908be9a73de40e654
RuiFCS <9150564@esmad.ipp.pt>
2020-11-28 18:54:21 +0000

    roulette

--- controllers/roulette.js
+++ controllers/roulette.js
@@ -6,8 +6,44 @@ const getRoulette = (req, res) => {
         if (err) {
             res.status(400).send(err); 
         }
-        res.status(200).json(roulette); 
+        res.status(200).json(rooms); 
     })
 }
 
-exports.getRoulette = getRoulette; 
+const getPoints = (req, res) => {
+    roulette.find(function(err, points){
+        if (err){
+            res.status(400).send(err, points);
+        }
+        numbers = []
+        probs = []
+
+        for (let index = 0; index < points[0].prizes.length; index++) {
+           let n = parseInt(points[0].prizes[index].value)
+           let p = parseFloat(points[0].prizes[index].probability)
+        numbers.push(n);
+        probs.push(p);
+        }
+        let valor = Math.random().choice(numbers, probs, size=1)
+        res.status(200).json(valor); 
+
+    })
+}
+
+/* const addRoulette = (req, res) =>{
+    const newRoulette = new roulette({
+        prizes: req.body.prizes,
+        items: req.body.cover,
+    })
+
+    newRoulette.save(function(err,museum){
+        if (err){
+            res.status(400).send(err);
+        }
+        res.status(200).json(museum)
+    })
+}
+ */
+exports.getRoulette = getRoulette;
+exports.getPoints = getPoints;
+/* exports.addRoulette = addRoulette;  */
--- models/roulette.js
+++ models/roulette.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const rouletteSchema = new mongoose.Schema({
-    prizes:Object,
+    prizes:Array,
     items: Array,
 })
 
-const rouletteSchema = mongoose.model('roulette', rouletteSchema)
+const roulette = mongoose.model('roulette', rouletteSchema)
 
 module.exports = roulette; 
