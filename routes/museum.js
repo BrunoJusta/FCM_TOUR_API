7c3acf1472092e6d2b533f207732430bd83d4ba8
RuiFCS <9150564@esmad.ipp.pt>
2020-11-28 15:54:26 +0000

    museum 1.1

--- routes/museum.js
+++ routes/museum.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/museum.js')
-
+const { body, validationResult, param} = require('express-validator');
 
 router.get('/museum', function (req, res) {
         const errors = validationResult(req);
@@ -11,6 +11,16 @@ router.get('/museum', function (req, res) {
             res.status(404).json({
                 errors: errors.array()
             })
+        }
+    })
+
+    router.post('/museum', [body('description').notEmpty().escape(),body('cover').notEmpty().escape()],
+    function (req, res){
+        const errors =  validationResult(req);
+        if (errors.isEmpty()){
+            controller.createRoom(req, res);
+        }else{
+            res.status(404).jsom({errors: errors.array()})
         }
     })
 
