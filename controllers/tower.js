f166f17ac8a8fcd3985910426e4e536474660974
Nuno Gomes <9180580@esmad.ipp.pt>
2020-11-29 14:39:39 +0000

    torre e livraria

---
+++ controllers/tower.js
@@ -0,0 +1,14 @@
+const tower = require('../models/tower.js')
+
+const getTowerRooms = (req, res) => {
+    tower.find({
+        tower
+    }, function (err, result) {
+        if (err) {
+            res.status(400).send(err);
+        }
+        res.status(200).json(result)
+    })
+}
+
+exports.getTowerRooms = getTowerRooms;
