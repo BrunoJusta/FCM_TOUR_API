03fe1ff3afa7fcb708803b477f53e1a5a818a456
BrunoJusta <brunomajusta@gmail.com>
2020-12-29 17:12:07 +0000

    w

--- controllers/users.js
+++ controllers/users.js
@@ -259,11 +259,11 @@ passport.use(new facebookStrategy({
     clientSecret: process.env.FB_SECRET,
     callbackURL: process.env.FB_CALLBACK_URL,
     profileFields: ["email", "name"]
-}, function (accessToken, refreshToken, profile, done) {
+}, function (req, accessToken, refreshToken, profile, done) {
     console.log('accessToken', accessToken)
     console.log('refreshToken', refreshToken)
     console.log('profile', profile)
-    console.log('url', callbackURL)
+    console.log('url', req.url)
 
     const data = profile._json;
     users.find({
