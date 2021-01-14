ea6b179851b2917f07bdd82d7cdac239b7e316c6
BrunoJusta <brunomajusta@gmail.com>
2021-01-14 17:35:30 +0000

    BILIOTECA

--- routes/library.js
+++ routes/library.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const controller = require('../controllers/library.js')
 const {
     body,
-    validationResult
+    validationResult,
+    param
 } = require('express-validator');
 
 router.get('/', function (req, res) {
@@ -13,6 +14,30 @@ router.get('/', function (req, res) {
     } else {
         res.status(404).json({
             errors: errors.array()
+        })
+    }
+})
+
+router.get('/acervos', function (req, res) {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        controller.getCollections(req, res);
+    } else {
+        res.status(404).json({
+            errors: errors.array()
+        })
+    }
+})
+
+router.get('/acervos/:id', [
+    param('id').notEmpty().escape()
+], function (req, res) {
+    const erros = validationResult(req);
+    if (erros.isEmpty()) {
+        controller.getCollectionsByID(req, res);
+    } else {
+        res.status(404).json({
+            errors: erros.array()
         })
     }
 })
