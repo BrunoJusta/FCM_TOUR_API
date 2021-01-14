27781bc4905e342aede3c4b0da7bf921542f7cc1
Nuno Gomes <9180580@esmad.ipp.pt>
2021-01-14 19:20:12 +0000

    change description on music

--- controllers/music.js
+++ controllers/music.js
@@ -9,7 +9,7 @@ const getCupertinos = (req, res) => {
         if (err) {
             res.status(400).send(err);
         } else {
-            speech.speeching(results[0].cupertinos.text, "cupertinos").then(result => {
+            speech.speeching(results[0].cupertinos.description, "cupertinos").then(result => {
                 if (result) {
                     music.findOne({
                         music
@@ -48,7 +48,7 @@ const getCiclos = (req, res) => {
         if (err) {
             res.status(400).send(err);
         } else {
-            speech.speeching(results[0].ciclos.text, "ciclos").then(result => {
+            speech.speeching(results[0].ciclos.description, "ciclos").then(result => {
                 if (result) {
                     music.findOne({
                         music
