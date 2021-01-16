eb419b6dca181858683b1b40b836cbb7d2cb3d06
BrunoJusta <brunomajusta@gmail.com>
2021-01-16 17:34:18 +0000

    s

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(function(req, res, next) {
 
 app.use(passport.initialize());
 app.use(express.json());
-app.use(auth)
+//app.use(auth)
 app.use('/', user)
 app.use('/museu', museum)
 app.use('/musica', music)
