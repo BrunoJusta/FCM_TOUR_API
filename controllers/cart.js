29a6636f6437999cc99a016c39638e72fa08823e
Nuno Gomes <9180580@esmad.ipp.pt>
2021-01-24 17:29:32 +0000

    delete from cart response

--- controllers/cart.js
+++ controllers/cart.js
@@ -4,13 +4,14 @@ const product = require('../models/products.js')
 
 const addToCart = (req, res) => {
 
-    product.find({number: req.params.product}, function (err, product) {
+    product.find({
+        number: req.params.product
+    }, function (err, product) {
         if (err) {
             res.status(400).send(err);
-        }
-        else{
-            console.log(product) 
-            
+        } else {
+            console.log(product)
+
             const cartToCreate = new cart({
                 email: req.params.email,
                 number: product[0].number,
@@ -19,23 +20,27 @@ const addToCart = (req, res) => {
                 img: product[0].img,
             });
 
-            cart.find({email: req.params.email, number:req.params.product},function (err, results) {
+            cart.find({
+                email: req.params.email,
+                number: req.params.product
+            }, function (err, results) {
                 if (err) {
                     res.status(400).send(err);
                 } else {
-                    if(results.length == 0){
+                    if (results.length == 0) {
                         cartToCreate.save(function (err, newCart) {
                             if (err) {
                                 res.status(400).send(err);
                             }
                             res.status(200).json({
-                                res: "Adicionado ao Carrinho!", state:1
+                                res: "Adicionado ao Carrinho!",
+                                state: 1
                             });
                         })
-                    }
-                    else{
+                    } else {
                         res.status(200).json({
-                            res: "Produto já adicionado ao Carrinho!", state:0
+                            res: "Produto já adicionado ao Carrinho!",
+                            state: 0
                         });
                     }
                 }
@@ -46,23 +51,29 @@ const addToCart = (req, res) => {
 
 
 const getCartByUser = (req, res) => {
-    cart.find({email: req.params.email}, function (err, result) {
+    cart.find({
+        email: req.params.email
+    }, function (err, result) {
         if (err) {
             res.status(400).send(err);
-        }
-        else{
+        } else {
             res.status(200).json(result);
         }
     })
 }
 
 const deleteProductInCart = (req, res) => {
-    cart.deleteOne({email: req.params.email, number:req.params.product}, function (err, result) {
+    cart.deleteOne({
+        email: req.params.email,
+        number: req.params.product
+    }, function (err, result) {
         if (err) {
             res.status(400).send(err);
-        }
-        else{
-            res.status(200).json("removido");
+        } else {
+            res.status(200).json({
+                res: "Produto Removido do carrinho!",
+                state: 0
+            });
         }
     })
 }
